946fcdc0162f9e0c5bde040ddf9ef09ff11563ee
ikhzz <fikriztm@gmail.com>
2020-11-03 20:35:03 +0800

    "upload firebase storage"

--- app/src/main/java/com/hollow/attendace_app/user/HomeFragment.kt
+++ app/src/main/java/com/hollow/attendace_app/user/HomeFragment.kt
@@ -13,21 +13,26 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.hollow.attendace_app.R
 import java.lang.IllegalArgumentException
-
+import java.util.*
 
 
 class HomeFragment: Fragment() {
 
     private lateinit var fStore : FirebaseDatabase
     private lateinit var imageUri : Uri
+    private lateinit var storageRef: FirebaseStorage
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
         return inflater.inflate(R.layout.home_fragment, container, false)
     }
 
@@ -36,11 +41,22 @@ class HomeFragment: Fragment() {
 
         val image : ImageView? = getView()?.findViewById(R.id.profPic)
         fStore = FirebaseDatabase.getInstance()
-        val sp: SharedPreferences? = context?.getSharedPreferences("data",Context.MODE_PRIVATE)
-        val str: String? = sp?.getString("url","")
-        if (str != "") {
-            setProfile()
+        storageRef = FirebaseStorage.getInstance()
+        val sp: SharedPreferences? = context?.getSharedPreferences("data", Context.MODE_PRIVATE)
+        val str: String? = sp?.getString("url", "")
+
+        if(!(str.isNullOrEmpty())) {
+            Toast.makeText(activity,"tes1",Toast.LENGTH_SHORT).show()
+            try {
+                Toast.makeText(activity,str,Toast.LENGTH_SHORT).show()
+                val url : Uri = Uri.parse(str)
+                image?.setImageURI(url)
+
+            } catch (e : IllegalArgumentException) {
+
+            }
         }
+
         image?.setOnClickListener{
             setImage()
         }
@@ -74,30 +90,48 @@ class HomeFragment: Fragment() {
             imageUri = data.data!!
 
             val image: ImageView? = view?.findViewById(R.id.profPic)
-            val sp: SharedPreferences? = context?.getSharedPreferences("data",Context.MODE_PRIVATE)
-            val edit = sp?.edit()
-            edit?.putString("url", imageUri.toString())
-            edit?.apply()
+
             image?.setImageURI(imageUri)
-
+            uploadImg()
         }
-        if(requestCode == 2) {
-            val sp: SharedPreferences? = context?.getSharedPreferences("data",Context.MODE_PRIVATE)
-            val str: String? = sp?.getString("url","")
-            val image : ImageView? = getView()?.findViewById(R.id.profPic)
-            try {
-                imageUri = Uri.parse(str)
-                image?.setImageURI(imageUri)
-            } catch (e : IllegalArgumentException) {
+//        if(requestCode == 2) {
+//            val sp: SharedPreferences? = context?.getSharedPreferences("data",Context.MODE_PRIVATE)
+//            val str: String? = sp?.getString("url","")
+//            val image : ImageView? = getView()?.findViewById(R.id.profPic)
+//            try {
+//                imageUri = Uri.parse(str)
+//                image?.setImageURI(imageUri)
+//            } catch (e : IllegalArgumentException) {
+//
+//            }
+//        }
+    }
+    private fun uploadImg() {
+        val imgName = "tes"
+        val ref: StorageReference = storageRef.reference.child("images/$imgName")
 
+        ref.putFile(imageUri)
+            .addOnSuccessListener { taskSnapshot -> // Get a URL to the uploaded content
+                val downloadUrl = taskSnapshot.uploadSessionUri?.path
+                val a = ref.downloadUrl.continueWith{
+                    tast ->
+                    val b = tast.result
+                    Toast.makeText(activity,b.toString(),Toast.LENGTH_SHORT).show()
+                    val sp: SharedPreferences? = context?.getSharedPreferences("data", Context.MODE_PRIVATE)
+                    val edit = sp?.edit()
+                    edit?.putString("url", b.toString())
+                    edit?.apply()
+                }
             }
-        }
+            .addOnFailureListener {
+                // Handle unsuccessful uploads
+                // ...
+            }
     }
-    private fun setProfile() {
-        val intent = Intent()
-        intent.type = "Uri"
-        intent.action = Intent.ACTION_OPEN_DOCUMENT
-        startActivityForResult(intent, 2)
-    }
-
+//    private fun setProfile() {
+//        val intent = Intent()
+//        intent.type = "Uri"
+//        intent.action = Intent.ACTION_OPEN_DOCUMENT
+//        startActivityForResult(intent, 2)
+//    }
 }
